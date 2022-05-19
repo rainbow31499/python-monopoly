98b0059761a6407087a833ce4415eb81b7347209
rainbow31499 <85587259+rainbow31499@users.noreply.github.com>
2022-05-19 16:18:58 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # This module starts the game
-
-from game import Game
-
-house_limited = True
-
-main_game = Game(house_limited=house_limited)
-
 try:
+    from game import Game
+
+    house_limited = True
+
+    main_game = Game(house_limited=house_limited)
+    
     main_game.main()
 except ModuleNotFoundError:
-    print('Module colorit not found. Please install module before running')
+    print('Module colorit not found. Please install module color-it before running')
     input()
